e84aaebd5515de2026d165e505fae0be040b54af
Roma <roma150102@gmail.com>
2018-04-19 23:02:06 +0300

    asfas
    asdas

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1524048088,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/1tx2-sheet0.png",
+		"images/background.png",
+		"images/gold_texure-sheet0.png",
+		"images/spawnpoint_gold-sheet0.png",
+		"images/elexir_texture-sheet0.png",
+		"images/shop_1-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tx-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
